0ac0b968787ec3d9ee01f1cfe33fa407bb5965ee
LenAnderson <Anderson.Len@outlook.com>
2023-03-24 13:28:21 +0100

    update scripts

--- scripts/update_mine.ps1
+++ scripts/update_mine.ps1
@@ -10,7 +10,7 @@ foreach ($part in $partList) {
 	$partDir = Join-Path $dst $part[1]
 	$childList = Get-ChildItem -Path (Join-Path $src $part[0]) -Recurse -File
 	foreach ($child in $childList) {
-		if ($child.Extension -ne '.rpyc' || $child.Name -eq 'README.md' || $child.Parent.Name -eq 'README') {
+		if (($child.Extension -ne '.rpyc') -and ($child.Name -ne 'README.md') -and ($child.Directory.Name -ne 'README') -and !(Test-Path (Join-Path $child.Directory.FullName '.skip'))) {
 			$childDst = Join-Path $partDir $child.FullName.Substring($src.FullName.Length + $part[0].Length + 1)
 			$childDstDir = $childDst.Substring(0, $childDst.Length - $child.Name.Length - 1)
 			if (!(Test-Path $childDstDir)) {
